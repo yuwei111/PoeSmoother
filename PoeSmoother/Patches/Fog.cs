18c8a4cd23ba983c387889280ceaa962638656aa
Gineticus <Bal3025@mail.ru>
2025-11-10 20:54:57 +0300

    changed fog patch

--- PoeSmoother/Patches/Fog.cs
+++ PoeSmoother/Patches/Fog.cs
@@ -11,43 +11,49 @@ public class Fog : IPatch
     public string Name => "Fog Patch";
     public object Description => "Disables the default fog effect in the game.";
 
+    private readonly string[] extensions = {
+        ".env",
+    };
+
+    private void RecursivePatcher(DirectoryNode dir)
+    {
+        foreach (var d in dir.Children)
+        {
+            if (d is DirectoryNode childDir)
+            {
+                RecursivePatcher(childDir);
+            }
+            else if (d is FileNode file)
+            {
+
+                if (Array.Exists(extensions, ext => file.Name.EndsWith(ext, StringComparison.OrdinalIgnoreCase)))
+                {
+                    var record = file.Record;
+                    var bytes = record.Read();
+                    string data = System.Text.Encoding.Unicode.GetString(bytes.ToArray());
+
+                    data = data.Replace("fog", "xog")
+                        .Replace("screenspace_xog", "screenspace_fog");
+
+                    var newBytes = System.Text.Encoding.Unicode.GetBytes(data);
+                    record.Write(newBytes);
+                }
+            }
+        }
+    }
+
     public void Apply(DirectoryNode root)
     {
-        // go to shaders/renderer/fog.ffx
+        // go to metadata/environmentsettings/
         foreach (var d in root.Children)
         {
-            if (d is DirectoryNode dir && dir.Name == "shaders")
+            if (d is DirectoryNode dir && dir.Name == "metadata")
             {
-                foreach (var dir1 in dir.Children)
+                foreach (var d2 in dir.Children)
                 {
-                    if (dir1 is DirectoryNode subDir && subDir.Name == "renderer")
+                    if (d2 is DirectoryNode subDir && subDir.Name == "environmentsettings")
                     {
-                        foreach (var dir2 in subDir.Children)
-                        {
-                            if (dir2 is FileNode file && file.Name == "fog.ffx")
-                            {
-                                var record = file.Record;
-                                var bytes = record.Read();
-                                string data = System.Text.Encoding.Unicode.GetString(bytes.ToArray());
-                                List<string> lines = data.Split("\r\n").ToList();
-                                for (int i = 0; i < lines.Count; i++)
-                                {
-                                    if (lines[i].Contains("oFogValue"))
-                                    {
-                                        if (lines[i].Contains("//"))
-                                            continue;
-                                        string trimmedLine = lines[i].TrimStart();
-                                        if (trimmedLine.StartsWith("oFogValue"))
-                                        {
-                                            lines[i] = "//" + lines[i];
-                                        }
-                                    }
-                                }
-                                string newData = string.Join("\r\n", lines);
-                                var newBytes = System.Text.Encoding.Unicode.GetBytes(newData);
-                                record.Write(newBytes);
-                            }
-                        }
+                        RecursivePatcher(subDir);
                     }
                 }
             }
